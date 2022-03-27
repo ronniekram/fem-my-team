867d192d824ccac18e9966ca45b4a019a3074c1c
Arynn Boniface <arynn.boniface@gmail.com>
2022-03-27 18:52:49 -0400

    feat: button types and styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,9 +9,37 @@ type ButtonProps = {
 };
 
 // ========== STYLES
+const baseStyles = [
+	tw`rounded-3xl`,
+	tw`font-semibold text-lg line-height[28px]`,
+	tw`transition duration-300 ease-in-out`,
+	tw`width[9.5625rem] height[3rem]`,
+	tw`disabled:(opacity-25)`,
+];
 
-// 153 x 48
-// border radius: 1.5rem
-// font: semi-bold, 18px line-height: 28px
+const light = [
+	tw`text-white bg-transparent`,
+	tw`border-2 border-white`,
+	tw`hover:(text-green-400 bg-white)`,
+];
+
+const dark = [
+	tw`text-green-400 bg-transparent`,
+	tw`border-2 border-green-400`,
+	tw`hover:(text-white bg-green-400)`,
+];
+
+const secondary = [
+	tw`bg-white text-green-400`,
+	tw`hover:(bg-blue-100)`,
+];
 
 // ========== COMPONENTS
+const Button = ({
+	label,
+	color,
+	href,
+	onClick,
+}: ButtonProps) => {};
+
+export default Button;
