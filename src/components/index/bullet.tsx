c4c5804e6ec6c2983db832992a1fbbe3653733d5
Arynn Boniface <arynn.boniface@gmail.com>
2022-03-29 20:12:13 -0400

    feat: bullet point component

---
+++ src/components/index/bullet.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import tw, { styled } from "twin.macro";
+import person from "../../assets/images/icons/icon-person.svg";
+import cog from "../../assets/images/icons/icon-cog.svg";
+import chart from "../../assets/images/icons/icon-chart.svg";
+
+// ========== TYPES ==========
+const icons = {
+	PERSON: person,
+	COG: cog,
+	CHART: chart,
+};
+
+type BulletProps = {
+	icon: keyof typeof icons;
+	title?: string;
+	content: string;
+};
+
+// ========== STYLES ==========
+const Contact = styled.div`
+	${tw`mx-auto`};
+	${tw`width[20.4375rem]`};
+	${tw`md:(width[32.1875rem])`};
+	${tw`xl:(width[33.75rem])`};
+	${tw`text-white text-lg font-bold line-height[28px]`};
+	${tw`flex items-center`};
+`;
+
+const Index = styled.div`
+	${tw`mx-auto`};
+	${tw`width[20.4375rem]`};
+	${tw`md:(width[35.8125rem])`};
+	${tw`xl:(width[33.75rem])`};
+	${tw`md:(flex items-center)`};
+	h2 {
+		${tw`text-coral text-lg font-bold line-height[28px] mb-2 md:(mb-4)`};
+	}
+	p {
+		${tw`text-white text-[15px] line-height[25px]`};
+	}
+`;
+
+// ========== COMPONENTS ==========
+const Bullet = ({ icon, title, content }: BulletProps) => {
+	return title ? (
+		<Index>
+			<div tw="md:(mr-6)">{icon}</div>
+			<div>
+				<h2>{title}</h2>
+				<p>{content}</p>
+			</div>
+		</Index>
+	) : (
+		<Contact>
+			<div tw="mr-6">{icon}</div>
+			<div>{content}</div>
+		</Contact>
+	);
+};
